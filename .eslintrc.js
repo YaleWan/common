87742d8c5742e2451b3b557196f05e04fab15bd8
yhwang <yale_wan@163.com>
2020-10-09 17:49:02 +0800

    chore: pre release sync

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,7 @@ module.exports = {
     es6: true
   },
   extends: ["eslint:recommended", "plugin:vue/essential"],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module"
-  },
+
   plugins: ["vue"],
   rules: {}
 };
